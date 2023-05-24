e7720288f9fe72ff842bdc069cecb296e58e11b2
Brayan Miza <bramizdev@gmail.com>
2023-05-24 09:24:56 -0600

    🛠️ Improve JS file

--- script.js
+++ script.js
@@ -1,58 +1,65 @@
 'use strict';
 
-const chart = document.querySelector('.chart');
+const $ = (selector) => document.querySelector(selector);
+
 const maxBarSize = '150';
 
-const getHighest = (arr) => {
+const $chart = $('.chart');
+
+const getHighestExpense = (arr) => {
   return arr.reduce(
     (acc, curr) => (acc = curr.amount > acc ? curr.amount : acc),
     0
   );
 };
 
-const calculatePercentaje = (highest, amount) => {
-  return (maxBarSize * amount) / highest;
+const calculateBarSize = (highest, amount) => {
+  return Math.trunc((maxBarSize * amount) / highest);
 };
 
-fetch('./data.json')
-  .then((response) => response.json())
-  .then((data) => {
-    const highestAmount = getHighest(data);
-    const updatedData = data.map((el) => {
-      if (el.amount !== highestAmount) return { ...el, highest: false };
-      return { ...el, highest: true };
-    });
+const displayExpenses = async (url) => {
+  const response = await fetch(url);
+  const expenses = await response.json();
 
-    updatedData.forEach(({ day, amount, highest }) => {
-      const markup = `
+  const highestAmount = getHighestExpense(expenses);
+
+  const updatedData = expenses.map((el) => {
+    if (el.amount !== highestAmount) return { ...el, highest: false };
+    return { ...el, highest: true };
+  });
+
+  updatedData.forEach(({ day, amount, highest }) => {
+    const markup = `
         <div class="chart__division" >
           <div
             data-amount="${amount}"
             class="chart__bar ${highest ? 'highest-day' : ''}"
             style="height:${
-              highest ? maxBarSize : calculatePercentaje(highestAmount, amount)
+              highest ? maxBarSize : calculateBarSize(highestAmount, amount)
             }px">
           </div>
           <p class="chart__day">${day}</p>
         </div>
       `;
-      chart.insertAdjacentHTML('beforeend', markup);
-    });
+    $chart.insertAdjacentHTML('beforeend', markup);
+  });
+};
 
-    chart.addEventListener('mouseover', (e) => {
-      const bar = e.target;
-      if (!bar.classList.contains('chart__bar')) return;
-      const markup = `
+displayExpenses('./data.json');
+
+$chart.addEventListener('mouseover', (e) => {
+  const bar = e.target;
+  if (!bar.classList.contains('chart__bar')) return;
+  const markup = `
         <span class="chart__span">$${bar.getAttribute('data-amount')}</span>
       `;
-      bar.insertAdjacentHTML('beforeend', markup);
-      bar.style.opacity = '0.8';
-    });
+  bar.insertAdjacentHTML('beforeend', markup);
+  bar.style.opacity = '0.75';
+});
 
-    chart.addEventListener('mouseout', (e) => {
-      const bar = e.target;
-      if (!bar.classList.contains('chart__bar')) return;
-      bar.innerHTML = '';
-      bar.style.opacity = '1';
-    });
-  });
+$chart.addEventListener('mouseout', (e) => {
+  const bar = e.target;
+  if (!bar.classList.contains('chart__bar')) return;
+  bar.innerHTML = '';
+  bar.style.opacity = '1';
+});
